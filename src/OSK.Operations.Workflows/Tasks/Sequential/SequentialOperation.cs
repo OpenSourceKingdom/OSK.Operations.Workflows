9eed2f75ad28fc1452111928a06af2f5d9613a52
BlankDev117 <mmmasent@gmail.com>
2026-09-20 22:26:03 -0400

    Add unit tests

--- src/OSK.Operations.Workflows/Tasks/Sequential/SequentialOperation.cs
+++ src/OSK.Operations.Workflows/Tasks/Sequential/SequentialOperation.cs
@@ -1,26 +1,56 @@
 ﻿using OSK.Operations.Workflows.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace OSK.Operations.Workflows.Tasks.Sequential;
 
 /// <summary>
 /// An operation that completes of a series sequential operation steps
 /// </summary>
-/// <param name="initialOperation">The initital operation to run</param>
-/// <param name="steps">Subsequent steps in the flow that have access to the prior step result</param>
-public class SequentialOperation(ITaskOperation initialOperation, params SequentialStep[] steps): IterativeOperation, ITaskOperation
+public class SequentialOperation: IterativeOperation, ITaskOperation
 {
     #region Variables
 
-    private ITaskOperation _currentOperation = initialOperation ?? throw new InvalidOperationException("Serquential steps requires an initial operation to run.");
+    private ITaskOperation _currentOperation;
+    private SequentialStep[] _steps;
+
     private int _currentStepIndex = -1;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// An operation that completes of a series sequential operation steps
+    /// </summary>
+    /// <param name="initialOperation">The initital operation to run</param>
+    /// <param name="steps">Subsequent steps in the flow that have access to the prior step result</param>
+    public SequentialOperation(ITaskOperation initialOperation, params SequentialStep[] steps)
+        : this(initialOperation, (IEnumerable<SequentialStep>)steps)
+    {
+    }
+
+    /// <summary>
+    /// An operation that completes of a series sequential operation steps
+    /// </summary>
+    /// <param name="initialOperation">The initital operation to run</param>
+    /// <param name="steps">Subsequent steps in the flow that have access to the prior step result</param>
+    public SequentialOperation(ITaskOperation initialOperation, IEnumerable<SequentialStep> steps)
+    {
+        _currentOperation = initialOperation ?? throw new InvalidOperationException("Serquential steps requires an initial operation to run.");
+        _steps = steps?.ToArray() ?? [];
+
+        TotalWorkItems = 1 + _steps.Length;
+    }
 
     #endregion
 
     #region IterativeOperation Overrides
 
     /// <inheritdoc/>
-    public override int TotalWorkItems { get; } = 1 + steps.Length;
+    public override int TotalWorkItems { get; }
 
     /// <inheritdoc/>
     protected override OperationStatus RunIteration(TimeSpan delta)
@@ -34,16 +64,18 @@ public class SequentialOperation(ITaskOperation initialOperation, params Sequent
         if (_currentOperation.IsFinished)
         {
             _currentStepIndex++;
-            if (_currentStepIndex >= steps.Length)
+            if (_currentStepIndex >= _steps.Length)
             {
                 return _currentOperation.Status;
             }
 
-            _currentOperation = steps[_currentStepIndex].TaskFactory(_currentOperation);
+            _currentOperation = _steps[_currentStepIndex].TaskFactory(_currentOperation);
             if (_currentOperation is null)
             {
                 return OperationStatus.Complete;
             }
+
+            return new OperationStatus(OperationState.InProgress, (1 + _currentStepIndex - 1) / TotalWorkItems);
         }
 
         return _currentOperation.Status;
--- src/OSK.Operations.Workflows/Tasks/Sequential/SequentialStep.cs
+++ src/OSK.Operations.Workflows/Tasks/Sequential/SequentialStep.cs
@@ -3,6 +3,10 @@ using System;
 
 namespace OSK.Operations.Workflows.Tasks.Sequential;
 
+/// <summary>
+/// Represents a single step in a sequential operation
+/// </summary>
+/// <param name="taskOperationFactory">The factory to create an operation</param>
 public class SequentialStep(Func<ITaskOperation, ITaskOperation> taskOperationFactory)
 {
     internal Func<ITaskOperation, ITaskOperation> TaskFactory => taskOperationFactory;
